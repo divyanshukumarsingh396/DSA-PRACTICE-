8026c43d5f003172e723a40e39fd843ce9171772
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-02-02 22:19:53 +0530

    divisibleby5.cpp

---
+++ divisibleby5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+
+    if(n%5==0){
+        cout<<"divisible ";
+    }
+    else {
+        cout<<"not divisible";
+    }
+
+    return 0;
+}
