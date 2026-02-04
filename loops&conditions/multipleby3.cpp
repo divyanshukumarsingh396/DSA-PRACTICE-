5e2336c6f107c1995c8bd06b25ae233d8ca890a5
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-02-04 21:43:19 +0530

    multiple by 3

---
+++ loops&conditions/multipleby3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+
+    if(n%3==0){
+        cout<<"multiple by 3";
+    }
+
+    else{
+        cout<<"not multiple";
+    }
+
+
+   
+
+    return 0;
+}
