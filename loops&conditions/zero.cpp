0877cd42c1903b88caa789b73f315dbe6f2d277e
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-02-04 21:41:28 +0530

    number is zero or non zero

---
+++ loops&conditions/zero.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"enter the number"<<endl;
+    cin>>number;
+
+    if(number==0)
+    {
+        cout<<"number is zero";
+    }
+else{
+    cout<<"non zero";
+}
+   
+
+    return 0;
+}
