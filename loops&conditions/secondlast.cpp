6f3d59a2945d93c4a41c7b3b6905554738cc287a
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-02-05 19:02:37 +0530

    Return Second Last Digit of the Given Number

---
+++ loops&conditions/secondlast.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int n= 234;
+
+     int secondlast = (n/10)%10;
+{  
+      cout<<" second last digit is : "<<secondlast;
+ }
+
+ return 0;
+}
