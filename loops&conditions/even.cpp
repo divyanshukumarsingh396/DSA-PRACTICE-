e2bbfcab51ef41a7c7a5c1c8d3e7d56980e78ef4
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-02-05 20:05:19 +0530

    of Given 5 Numbers, How Many Are Even?

---
+++ loops&conditions/even.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int a,b,c,d,e;
+int count=0;
+ cout<<"enter the numbers : ";
+ cin>>a>>b>>c>>d>>e;
+
+ if(a % 2 == 0) count++;
+    if(b % 2 == 0) count++;
+    if(c % 2 == 0) count++;
+    if(d % 2 == 0) count++;
+    if(e % 2 == 0) count++;
+  
+    cout<<" Numbers of even "<<count;
+ 
+
+ return 0;
+}
