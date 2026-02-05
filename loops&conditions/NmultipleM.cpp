a9ed603761187b672e988a63d57a6e1f009e138b
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-02-05 21:41:34 +0530

    Is N an Exact Multiple of M

---
+++ loops&conditions/NmultipleM.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int N ,M;
+ cout<<"Enter the values :";
+ cin>>N>>M;
+
+ if(N%M==0){
+cout<<"N multiple of M";
+
+ }
+ else {
+  cout<<" is not";
+ }
+
+ return 0;
+}
