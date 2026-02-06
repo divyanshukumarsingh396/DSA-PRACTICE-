f96fcce3135cdc189f0c74cd83d9d9683ecb2d5e
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-02-06 19:17:07 +0530

    Update passfail.cpp

--- loops&conditions/passfail.cpp
+++ loops&conditions/passfail.cpp
@@ -1,1 +1,32 @@
+pass fail
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"enter the number"<<endl;
+    cin>>num;
+
+    if(num>=90){
+        cout<<"A grade";
+    }
+    else if(num>=80){
+        cout<<"B grade";
+    }
+
+else if(num>=60){
+    cout<<"C grade";
+}
+else if(num>=40){
+    cout<<"D grade";
+}
+
+else{
+    cout<<"fail";
+}
+   
+    return 0;
+}
+
 
