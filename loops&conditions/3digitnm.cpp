2abb832900788dde02e63280f574c473cbd0e9e1
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-02-07 01:35:54 +0530

    3 digit number or not

---
+++ loops&conditions/3digitnm.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"enter the number"<<endl;
+    cin>>num;
+
+    if(num<1000)
+{
+    cout<<"3 digit num";
+}
+else{
+    cout<<"not";
+}
+
+   
+    return 0;
+}
+
