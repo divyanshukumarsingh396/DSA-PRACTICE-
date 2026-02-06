be6e0667fde7a206ed48b09c11c6f241ae9c7c5f
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-02-06 19:18:30 +0530

    positive negative zero

---
+++ loops&conditions/neg.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"enter the number"<<endl;
+    cin>>num;
+
+   if(num>=1){
+    cout<<"positive number";
+   }
+   else if(num==0){
+    cout<<"zero";
+   }
+   else{
+    cout<<"negative";
+   }
+   
+    return 0;
+}
+
