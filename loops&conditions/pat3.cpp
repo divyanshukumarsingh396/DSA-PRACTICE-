d0fabc76ccec2155023d0f9f900824af0ad14cf1
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-02-09 20:03:43 +0530

    Create pat3.cpp

---
+++ loops&conditions/pat3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+   
+    for(int i=0; i<13; i++){
+        for(int j=0; j<=i-5+1; j++){
+
+           for(int k=0; k<=j-1; k++){
+            cout<<" ";
+
+           } cout<<" ";
+        }
+       cout<<"*"<< endl;
+    }
+
+ return 0;
+}
