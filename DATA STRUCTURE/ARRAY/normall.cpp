5cd37c2004c4825a1558e4f2b6711ad296791c89
Divyanshu kumar singh <divyanshukumarsingh396@gmail.com>
2026-03-05 00:42:19 +0530

    normal marks print

--- DATA STRUCTURE/ARRAY/normall.cpp
+++ DATA STRUCTURE/ARRAY/normall.cpp
@@ -1,29 +1,25 @@
 #include<iostream>
 using namespace std;
+
+int main (){
+
+
+
+int marks[5]={ 5,554,44,56,54};
+
+double num[]= {99.7, 76, 86};
+
+marks[1]= 123;
+cout<<marks[0]<<endl;
+cout<<marks[1]<<endl;
+cout<<marks[2]<<endl;
+cout<<marks[3]<<endl;
+cout<<marks[4]<<endl;
+
  
-void even();
-void odd();
+return 0;
 
-int  n=1;
-void odd(){
-
-
-    if(n <=10){
-        cout<< n+1<<" ";
-        n++;
-        even();
-    }
-    return ;   
-}
-void even(){
-
-
-    if(n <=10){
-        cout<<n-1<<" ";
-        n++;
-        odd();
-    }
-    return ;   
+}    return ;   
 }
 int main(){
     odd();
