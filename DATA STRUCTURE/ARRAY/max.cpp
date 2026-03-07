1d5b10a5345b0d57aadb656e9c06b4e6f194582f
Divyanshu Kumar <divyanshukumarsingh396@gmail.com>
2026-03-07 15:47:55 +0530

    ADD NEW

--- DATA STRUCTURE/ARRAY/max.cpp
+++ DATA STRUCTURE/ARRAY/max.cpp
@@ -3,18 +3,29 @@ using namespace std;
 
 int main(){
 
-    int num[]={32,23,-22,42,12,3};
+    int num[]={32,23,-22,254,12,33};
 
 int size = 6;
 
+int Smallest = INT8_MAX;
  int Largest = INT8_MIN;
 
  for(int i=0; i<size; i++){
-    if(num[i]>Largest){
-        Largest =num[i];
-    }
+    Smallest = min(num[i], Smallest);
+    Largest = max(num[i], Largest);
+    
  }
- cout<<"smallest = "<<Largest<<endl;
+ cout<<"smallest = "<<Smallest<<endl;
+ cout<<"largest = "<<Largest<<endl;
+
+     for(int i = 0; i < size; i++) {
+        if(num[i] == Smallest)
+            cout << "Min index = " << i << endl;
+
+        if(num[i] == Largest)
+            cout << "Max index = " << i << endl;
+    }
+
 
 return 0;
 }
